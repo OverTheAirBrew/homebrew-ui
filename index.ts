f2ec21fa82252a4d2f2814929172f0f232713e6f
Nick Sharp <nick.sharp.87@gmail.com>
2021-07-05 20:12:46 +0100

    fix: changing next

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import next from 'next';
+import { join } from 'path';
+
+export function initUi(app: Application) {
+  const nextApp = next({
+    dir: join(__dirname),
+  });
+
+  const handler = nextApp.getRequestHandler();
+
+  app.all('*', (req: Request, res: Response) => {
+    return handler(req, res);
+  });
+}
